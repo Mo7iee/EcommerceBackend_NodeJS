6c54b9bb5ce84853c99550dc70f2d86ff9cf6bb5
Mohie-Eldin Mohamed <Mohie707034@gmail.com>
2025-09-27 15:44:06 +0300

    Login&Register Logic + Endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoute from "./routes/userRoute.js";
 
 const app = express();
 const port = 3001;
 
+app.use(express.json())
+
 mongoose
-  .connect("mongodb://localhost:27017/")
+  .connect("mongodb://localhost:27017/ecommerce")
   .then(() => console.log("Mongo Connected!"));
+
+  app.use('/user',userRoute)
 
 app.listen(port, () => {
   console.log("Server is running");
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,30 @@
 import type { RegisterDTO } from "./DTOs/registerDTO.ts";
 import type { LoginDTO } from "./DTOs/loginDTO.ts";
+import { userModel } from "../models/userModel.js";
 
+export const register = async ({ firstName, lastName, email, password }: RegisterDTO) => {
+  const findUser = await userModel.findOne({ email });
 
+  if (findUser) {
+    return { data: "User already exists!", statusCode: 400 };
+  }
+
+  const newUser = new userModel({ firstName, lastName, email, password });
+  await newUser.save();
+
+  return { data: newUser, statusCode: 200 };
+};
+export const login = async ({email,password}: LoginDTO) => {
+    const findUser = await userModel.findOne({email})
+    if(!findUser) {
+        return { data:"Incorrect email or password!", statusCode: 400 };
+    }
+
+    const passwordMatch = password === findUser.password;
+    if(passwordMatch){
+        return { data: findUser, statusCode: 200 };
+    }
+        return { data: "Incorrect email or password!", statusCode: 400 };
+   
+
+}
