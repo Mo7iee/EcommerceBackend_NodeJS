aa8035daf88d763ce048117798f0cc35e37631d2
Mohie-Eldin Mohamed <Mohie707034@gmail.com>
2025-09-30 14:50:47 +0300

    Password encryption using Bcrypt

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,7 @@
 import type { RegisterDTO } from "./DTOs/registerDTO.ts";
 import type { LoginDTO } from "./DTOs/loginDTO.ts";
 import { userModel } from "../models/userModel.js";
+import bcrypt from 'bcrypt';
 
 export const register = async ({ firstName, lastName, email, password }: RegisterDTO) => {
   const findUser = await userModel.findOne({ email });
@@ -8,8 +9,8 @@ export const register = async ({ firstName, lastName, email, password }: Registe
   if (findUser) {
     return { data: "User already exists!", statusCode: 400 };
   }
-
-  const newUser = new userModel({ firstName, lastName, email, password });
+  const hashedPassword = await bcrypt.hash(password,10)
+  const newUser = new userModel({ firstName, lastName, email, password: hashedPassword });
   await newUser.save();
 
   return { data: newUser, statusCode: 200 };
@@ -20,7 +21,7 @@ export const login = async ({email,password}: LoginDTO) => {
         return { data:"Incorrect email or password!", statusCode: 400 };
     }
 
-    const passwordMatch = password === findUser.password;
+    const passwordMatch = await bcrypt.compare(password, findUser.password);
     if(passwordMatch){
         return { data: findUser, statusCode: 200 };
     }
