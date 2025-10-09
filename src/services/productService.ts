6f16dab9b8a1c6387996978f1b226c95dcb1e785
Mohie-Eldin Mohamed <Mohie707034@gmail.com>
2025-10-09 14:16:56 +0300

    Products CRUD

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -4,6 +4,18 @@ export const getAllProducts = async () => {
   return await productModel.find();
 };
 
+export const getProductById = async (_id: any) => {
+  return await productModel.findById(_id);
+};
+export const deleteProduct = async (_id: any) => {
+  return await productModel.findByIdAndDelete(_id);
+};
+export const updateProduct = async (_id:any, updatedData: any) => {
+  return await productModel.findByIdAndUpdate(_id, updatedData, {
+    new: true,         
+    runValidators: true 
+  });
+};
 export const seedProducts = async () => {
   const products = [
     { title: "Product 1", image: "img1.jpg", price: 10, stock: 100 },
