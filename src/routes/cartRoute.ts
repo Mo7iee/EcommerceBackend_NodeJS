38df55b7ea02d479a361c3deda572c6faec3cfb7
Mohie-Eldin Mohamed <Mohie707034@gmail.com>
2025-10-10 14:39:44 +0300

    addItemToCartDTO + Endpoint

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { getActiveCart } from "../services/cartService.js";
+import { addItemToCart, getActiveCart } from "../services/cartService.js";
 import validateJWT from "../middlewares/validateJWT.js";
 import type { ExtendedRequest } from "../middlewares/validateJWT.js";
+import { getProductById } from "../services/productService.js";
 
 const router = express.Router()
 
@@ -10,7 +11,14 @@ router.get("/", validateJWT, async (req: ExtendedRequest, res) => {
   const cart = await getActiveCart({ userId });
   res.status(200).send(cart);
 });
+router.post("/items", validateJWT, async (req: ExtendedRequest, res) => {
+  const userId = req.user?._id;
+  const { productId, quantity } = req.body;
 
+  const response = await addItemToCart({ userId, productId, quantity });
+    res.status(response.statusCode).send(response.data);
 
+}
+)
 export default router;
 
--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,5 +1,7 @@
 import { cartModel } from "../models/cartModel.js";
 import type { cartDTO } from "./DTOs/cartDTO.ts";
+import type { addItemToCartDTO } from "./DTOs/addItemToCartDTO.ts";
+import { productModel } from "../models/productModel.js";
 
 const createCart = async ({ userId }:cartDTO) =>{
     const cart = await cartModel.create({userId})
@@ -14,3 +16,21 @@ export const getActiveCart = async ({ userId }:cartDTO) =>{
     }
     return cart;
 }
+export const addItemToCart = async ({ productId,quantity,userId }:addItemToCartDTO) =>
+    {
+        const cart = await getActiveCart({userId})
+        const existsInCart = cart?.items.find((p) => p.product === productId);
+        if(existsInCart){
+            return{data:"Product already exists in cart",statusCode:400}
+        }
+        const product = await productModel.findById(productId);
+        if(!product){
+             return{data:"Product not found",statusCode:400}
+
+
+        }
+        cart?.items.push({product:productId,unitPrice:product.price,quantity: parseInt(quantity)})
+        const updatedCart = await cart?.save();
+
+        return { data: updatedCart, statusCode:200}
+    }
